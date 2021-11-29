bfc67e2243af2e38f0e878090ba103a7eb6d5988
Nathan Keeys <nathankeeys@Nathans-MacBook-Pro.local>
2021-11-29 14:08:51 -0500

    push

--- pages/about.js
+++ pages/about.js
@@ -17,10 +17,10 @@ const about = () => {
 				textDescription="The studio was founded in 2018 by Creative Technologist, Nathan Keeys due to the increasing complexities in front-end web development and user experience design. Digital Anthro continues to grow and explore connections between design, technology, and digital human behavior."
 			></PageTitle>
 			<div clasName="social-media">
-				<a target="_blank" href="https://www.linkedin.com/company/digital-anthro">
+				<a target="_blank" rel="noreferrer" href="https://www.linkedin.com/company/digital-anthro">
 					LinkedIn
 				</a>
-				<a target="_blank" href="https://www.instagram.com/digitalanthro/">
+				<a target="_blank" rel="noreferrer" href="https://www.instagram.com/digitalanthro/">
 					Instagram
 				</a>
 			</div>
