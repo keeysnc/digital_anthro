a8bade4a6fdf084670f311d9f7afdd70565a665e
Nathan Keeys <nathankeeys@Nathans-MacBook-Pro.local>
2022-01-31 07:05:53 -0500

    update services again

--- components/paragraphText/ParagraphText.js
+++ components/paragraphText/ParagraphText.js
@@ -4,7 +4,7 @@ const ParagraphText = ({ textTitle, textDescription, link }) => {
 			<h3>{textTitle ? textTitle : ""}</h3>
 			<p>{textDescription}</p>
 			{link ? (
-				<a href={link} rel="noreferrer" target="_blank">
+				<a style={{ textDecoration: "underline" }} href={link} rel="noreferrer" target="_blank">
 					Learn More
 				</a>
 			) : null}
--- pages/services.js
+++ pages/services.js
@@ -13,40 +13,42 @@ const services = () => {
 			<div className={serviceStyles.services__grid}>
 				<div className={serviceStyles.services__grid__item}>
 					<ParagraphText
-						textTitle="Interactive Web Development"
-						textDescription="We build interactive experiences & websites utilizing full-stack javascript development and modern web practices."
+						textTitle="Javascript Web Development"
+						textDescription="Digital Anthro builds interactive experiences, websites, stores and apps utilizing full-stack javascript development and modern web practices."
 					></ParagraphText>
 				</div>
 				<div className={serviceStyles.services__grid__item}>
 					<ParagraphText
-						textTitle="Web Design & Development"
-						textDescription="Digital Anthro designs and builds microsites, landing pages and interactive one pagers for brands, organizations and start-ups."
+						textTitle="Headless CMS Implementation"
+						textDescription="Digital Anthro works with clients who are in need of easily maintainable and flexible content management system solutions."
+						link="https://www.sanity.io/blog/headless-cms-explained"
 					></ParagraphText>
 				</div>
 				<div className={serviceStyles.services__grid__item}>
 					<ParagraphText
-						textTitle="UI/UX & Interaction Design"
-						textDescription="We specialize in designing user interfaces for digital experiences using modern component-based approaches for scalability and flexibility."
+						textTitle="Front-End Web Development Consulting"
+						textDescription="Digital Anthro specializes in architecting UI and data flow for front-end solutions using the modern frameworks such as React and Vue.js."
+					></ParagraphText>
+				</div>
+				<div className={serviceStyles.services__grid__item}>
+					<ParagraphText
+						textTitle="Visual Design"
+						textDescription="Digital Anthro works with clients from the initial design exploration phase to implementation of website or app redesigns."
+					></ParagraphText>
+				</div>
+				<div className={serviceStyles.services__grid__item}>
+					<ParagraphText
+						textTitle="UI/UX/Interaction Strategy & Design"
+						textDescription="Digital Anthro specializes in building design systems for websites and apps using modern component-based approaches for scalability and flexibility."
 					></ParagraphText>
 				</div>
 				<div className={serviceStyles.services__grid__item}>
 					<ParagraphText
 						textTitle="Creative Development"
-						textDescription="We storyboard, design, prototype and execute front-end solutions for immersive websites utilizing elements such as 3D rendering, animation, audio or video."
+						textDescription="Digital Anthro works with clients in need of implementing highly interactive components for their website such as web animations, visual effects, data viz, 3D product customizers and more."
 					></ParagraphText>
 				</div>
-				<div className={serviceStyles.services__grid__item}>
-					<ParagraphText
-						textTitle="Data Visualization Websites"
-						textDescription="We work with clients who need to visualize or create an interactive web experience using data or content."
-					></ParagraphText>
-				</div>
-				<div className={serviceStyles.services__grid__item}>
-					<ParagraphText
-						textTitle="WebGL Development"
-						textDescription="Digital Anthro specializes in 3D web development. We utilize WebGL technologies to create 3D experiences for our clients."
-					></ParagraphText>
-				</div>
+
 				{/* <div className={serviceStyles.services__grid__item}>
 				<ParagraphText
 					textTitle="Interactive Installations"
