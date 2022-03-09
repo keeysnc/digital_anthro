0b52051307b25a6273c5bfbd6915766715183c66
Nathan Keeys <nathankeeys@Nathans-MacBook-Pro.local>
2022-03-09 14:38:20 -0500

    about description

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,7 @@ const about = () => {
 			<PageTitle
 				title="About"
 				textTitle=""
-				textDescription="Digital Anthro is a creative studio, interactive development consultancy and Shopify Partner. The studio was founded in 2018 due to the emergence and maturity of interactive and immersive technologies on the web. Digital Anthro's mission is one of design, technology and human behavior. The mission is simply to explore modern methods of web interaction and how they can better the digital human experience."
+				textDescription="Digital Anthro is a creative studio, interactive web development consultancy and Shopify Partner. The studio was founded in 2018 due to the emergence and maturity of interactive and immersive technologies on the web. Digital Anthro's mission is one of design, technology and human behavior. The mission is simply to explore modern methods of web interaction and how they can better the digital human experience."
 			></PageTitle>
 			<div className="social-media" style={{ textAlign: "center", paddingTop: "10px" }}>
 				<a target="_blank" rel="noreferrer" href="https://www.linkedin.com/company/digital-anthro">
