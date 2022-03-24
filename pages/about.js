86cbe0218e5a034701e00ddddf3435616e5ee9c5
Nathan Keeys <nathankeeys@Nathans-MacBook-Pro.local>
2022-03-24 13:37:02 -0400

    update verbage

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import PageTitle from "../components/pageTitle/PageTitle";
-import ParagraphText from "../components/paragraphText/ParagraphText";
 import aboutStyles from "../styles/About.module.css";
 import indexStyles from "../styles/Index.module.css";
 
@@ -16,8 +14,8 @@ const about = () => {
 						<h1>About</h1>
 						<h3>
 							Digital Anthro is a creative studio, interactive development consultancy. The studio was founded in 2018 due to the emergence and
-							maturity of interactive and immersive technologies on the web. Digital Anthro's mission is one of design, technology and human behavior.
-							The mission is simply to explore modern methods of web interaction and how they can better the digital human experience."
+							maturity of interactive and immersive technologies on the web. The Digital Anthro mission is one of design, art, technology and human
+							behavior. The mission is simply to explore modern methods of web interaction and how they can better the digital human experience.
 						</h3>
 						<div className="social-media" style={{ textAlign: "center", paddingTop: "10px" }}>
 							<a target="_blank" rel="noreferrer" href="https://www.linkedin.com/company/digital-anthro">
