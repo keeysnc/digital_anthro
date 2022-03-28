ddaba58663fcf91b604ab907704e6ec10588fa0a
Nathan Keeys <nathankeeys@Nathans-MacBook-Pro.local>
2022-03-28 17:58:29 -0400

    service update

--- pages/services.js
+++ pages/services.js
@@ -13,9 +13,9 @@ const services = () => {
 				</Head>
 				<div style={{ textAlign: "center" }}>
 					<h1>Services & Technologies</h1>
-					<h3>Javascript Web & Mobile Development</h3>
-					<h3>WebGL & 3D Web Development</h3>
-					<h3>D3.js & WebGL Data Visualizations</h3>
+					<h3>Web Design & Development</h3>
+					<h3>3D Web Development</h3>
+					<h3>Data Visualizations</h3>
 					<h3>UI / UX / Interaction Design</h3>
 					<h3>3D Modeling and Animation</h3>
 				</div>
