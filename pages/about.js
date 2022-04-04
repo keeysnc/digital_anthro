64ccf1941ffe4369d356c5d0f083eadb6aadce36
Nathan Keeys <nathankeeys@Nathans-MacBook-Pro.local>
2022-04-04 16:20:37 -0400

    update

--- pages/about.js
+++ pages/about.js
@@ -14,9 +14,9 @@ const about = () => {
 						<h1>About</h1>
 						<h3>
 							Digital Anthro is a web design & development consultancy specialized in immersive, human-centered websites & digital experiences for
-							lifestyle, wellness, and health brands and organizations. The studio was founded in 2018 to help forward-thinking health & wellness
-							companies build creative digital experiences for their future & existing customers. The Digital Anthro mission is one of design, art,
-							technology and human behavior. The mission is simply to design and build impactful user experiences for the digital human.
+							lifestyle, wellness, and health brands and organizations. The studio was founded in 2018 to help forward-thinking lifestyle, health &
+							wellness companies build creative digital experiences for their future & existing customers. The Digital Anthro mission is one of
+							design, art, technology and human behavior. The mission is simply to design and build impactful user experiences for the digital human.
 						</h3>
 						<div className="social-media" style={{ textAlign: "center", paddingTop: "10px" }}>
 							<a target="_blank" rel="noreferrer" href="https://www.linkedin.com/company/digital-anthro">
