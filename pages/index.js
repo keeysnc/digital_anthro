626142966a91aabb59b6ba6152ebb54756001a77
Nathan Keeys <nathankeeys@Nathans-MacBook-Pro.local>
2022-09-06 16:05:36 -0400

    grammar edit

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,9 @@ export default function Home() {
 				<div className={indexStyles.home__content}>
 					<h1>Digital Anthro</h1>
 					<p>
-						Digital Anthro is an independent research & design studio specialized in human-centered design and development web, mobile & physical
+						Digital Anthro is an independent research & design studio specialized in human-centered design and development for web, mobile & physical
 						spaces. The studio was founded in 2018 to help forward-thinking brands, organizations and institutions build interactive data-driven
-						experiences in digital and physical enviornments. The Digital Anthro mission is one of design, technology and human behavior. The mission
+						experiences in digital and physical environments. The Digital Anthro mission is one of design, technology and human behavior. The mission
 						is simply to design and build impactful user experiences.
 					</p>
 					<br />
