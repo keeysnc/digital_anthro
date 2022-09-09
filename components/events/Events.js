92081cdd5a8f53ff579b94ba96ec2ddd2bfb51f2
Nathan Keeys <nathankeeys@Nathans-MacBook-Pro.local>
2022-09-08 23:27:53 -0400

    update events

--- components/events/Events.js
+++ components/events/Events.js
@@ -4,11 +4,7 @@ const Events = () => {
 	return (
 		<div>
 			<hr />
-			<EventItem
-				eventTitle="Intro to Touchdesigner Workshop"
-				eventDate="COMING SOON"
-				eventDescription="Touchdesigner is a visual node-based programming language and platform for creating interactive experiences. Digital Anthro goes through the primary basics of how to navigate Touchdesigner and use all the built in operators to create experimental art."
-			></EventItem>
+			<EventItem eventTitle="Events Coming Soon!" eventDate="N/A" eventDescription="N/A"></EventItem>
 			<hr />
 		</div>
 	);
