060df96921ae03bcf748b18ddf6c4c6e0de3079c
Nathan Keeys <nathankeeys@nathans-mbp.myfiosgateway.com>
2022-09-12 21:42:37 -0400

    update

--- pages/work/mednovate.js
+++ pages/work/mednovate.js
@@ -6,7 +6,7 @@ const mednovate = ({ contentData, allPostsData }) => {
 	return (
 		<div>
 			<div className={caseStyles.case__hero}>
-				<img style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "top" }} src={allPostsData[2].image} />
+				<img style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "top" }} src={allPostsData[1].image} />
 			</div>
 			<div className={caseStyles.case__study} dangerouslySetInnerHTML={{ __html: contentData.contentHtml }}></div>
 		</div>
